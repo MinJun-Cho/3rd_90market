2ec96547f39a0deca83d244c8100249685012921
Ji-GOO <j22sooj22soo@gmail.com>
2020-04-13 14:04:03 +0900

    oauth naver add

--- src/main/java/com/googongmarket/controller/AuthController.java
+++ src/main/java/com/googongmarket/controller/AuthController.java
@@ -1,18 +1,113 @@
 package com.googongmarket.controller;
 
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.github.scribejava.core.model.OAuth2AccessToken;
+import com.googongmarket.domain.MemberVO;
+import com.googongmarket.domain.NaverLoginBO;
+import com.googongmarket.mapper.AuthMapper;
+import com.googongmarket.service.MemberService;
+
+import lombok.Setter;
 
 @Controller
 public class AuthController {
 
-//	/* NaverLoginBO */
-//	private NaverLoginBO naverLoginBO;
-//	private String apiResult = null;
-//	
-//	@Autowired
-//	private void setNaverLoginBO(NaverLoginBO naverLoginBO) {
-//		
-//		this.naverLoginBO = naverLoginBO;
-//	}
+	@Setter(onMethod_ = {@Autowired})
+	private MemberService service;
+	
+	@Setter(onMethod_ = {@Autowired})
+	private AuthMapper mapper;
+	private NaverLoginBO naverLoginBO;
+	private String naverProfile = null;
+	
+	@Resource(name = "loginMember")
+	@Lazy
+	private MemberVO loginMember;
 
+	@Autowired
+	private void setNaverLoginBO(NaverLoginBO naverLoginBO) {
+		
+		this.naverLoginBO = naverLoginBO;
+	}
+	
+	@GetMapping("/auth/naver/login")
+	public String naverLogin(Model model, HttpSession session) {
+		
+		String naverAuthUrl = naverLoginBO.getAuthorizeationUrl(session);
+		
+		//System.out.println("네이버 : " + naverAuthUrl);
+		
+		model.addAttribute("url", naverAuthUrl);
+		
+		return "auth/naver/login";
+	}
+	
+	@GetMapping("/auth/naver/callback")
+	public String callback(Model model, @RequestParam String code, @RequestParam String state, HttpSession session,
+							HttpServletRequest request, MemberVO memberVO) throws IOException, InterruptedException, ExecutionException, ParseException {
+		
+		//System.out.println("여기는 callback");
+		
+		OAuth2AccessToken oauthToken;
+		oauthToken = naverLoginBO.getAccessToken(session, code, state);
+		//System.out.println("\noauthToken : " + oauthToken + "\n");
+		
+		naverProfile = naverLoginBO.getUserProfile(oauthToken);
+		
+		//System.out.println(naverLoginBO.getUserProfile(oauthToken).toString());
+		
+		JSONParser jsonParser = new JSONParser();
+		JSONObject jsonObject =(JSONObject) jsonParser.parse(naverProfile.toString());
+		JSONObject response = (JSONObject) jsonObject.get("response");
+		
+		//System.out.println("response : " + response);
+		
+		memberVO.setEmail((String) response.get("email"));
+		memberVO.setUsername((String) response.get("name"));
+		memberVO.setNickname((String) response.get("nickname"));
+		memberVO.setPasswd("NAVER");
+		
+		//System.out.println(service.emailCheck((String) response.get("email")));
+		
+		if(service.emailCheck((String) response.get("email"))) {
+			
+			mapper.joinAuthMember(memberVO);
+			int id = mapper.idCheck(memberVO.getEmail());
+			//System.out.println("id : " + id);
+			
+			//service.joinMember(memberVO);
+			
+			loginMember.setId(id);
+			loginMember.setEmail(memberVO.getEmail());
+			loginMember.setMemberLogin(true);
+		} else {
+			
+			int id = mapper.idCheck(memberVO.getEmail());
+			
+			loginMember.setId(id);
+			loginMember.setEmail(memberVO.getEmail());
+			loginMember.setMemberLogin(true);
+		}
+		
+		model.addAttribute("result", naverProfile);
+		//System.out.println("\nresult : " + naverProfile + "\n");
+		
+		return "auth/naver/success";
+	}
 }
--- src/main/webapp/WEB-INF/views/join.jsp
+++ src/main/webapp/WEB-INF/views/join.jsp
@@ -3,13 +3,12 @@
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <!DOCTYPE html>
 <html>
-
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>회원가입</title>
-	<link rel="shortcut icon" type="image/x-icon" href="img/logo2.png" />
-	<link rel="stylesheet" href="css/login.css">
+	<link rel="shortcut icon" type="image/x-icon" href="/resources/img/logo2.png" />
+	<link rel="stylesheet" href="/resources/css/login.css">
 	<script src="https://kit.fontawesome.com/7b5ecf7db0.js" crossorigin="anonymous"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
@@ -47,36 +46,37 @@
 		$("#emailExist").val("false")
 	}
 </script>
-
 <body>
 	<div class = 'member'>
         <div class = 'member_container'>
 			<h2>회원가입</h2>
-	<form:form action="/postJoin" method="post" modelAttribute="memberVO">
-		<form:hidden path="emailExist" />
-		이메일 :
-		<form:input type="text" path="email" onkeypress="resetEmailExist()" />
-		<button type="button"  id='overlap' onclick="emailCheck()">중복확인</button><br>
-		<form:errors path="email" style="color:red" /><br><br>
-		패스워드 :
-		<form:password showPassword="true" path="passwd" /><br>
-		<form:errors path="passwd" style="color:red" /><br><br>
-		패스워드 확인 :
-		<form:password showPassword="true" path="passwd2" /><br>
-		<form:errors path="passwd2" style="color:red" /><br><br>
-		성명 :
-		<form:input type="text" path="username" maxlength="10" /><br>
-		<form:errors path="username" style="color:red" /><br><br>
-		닉네임 :
-		<form:input type="text" path="nickname" /><br>
-		<form:errors path="nickname" style="color:red" /><br><br>
-		연락처 :
-		<form:input type="text" path="phone" /><br>
-		<form:errors path="phone" style="color:red" /><br><br>
-
-		<form:button type="submit">회원가입</form:button>
-		<input type="button" value="취소" onclick="location.href='/'" />
-	</form:form>
+				<form:form action="/postJoin" method="post" modelAttribute="memberVO">
+					<form:hidden path="emailExist" />
+					이메일 : 
+					<form:input type="text" path="email" onkeypress="resetEmailExist()" />
+					<button type="button"  id='overlap' onclick="emailCheck()">중복확인</button><br>
+					<form:errors path="email" style="color:red" /><br><br>
+					패스워드 :
+					<form:password showPassword="true" path="passwd" /><br>
+					<form:errors path="passwd" style="color:red" /><br><br>
+					패스워드 확인 :
+					<form:password showPassword="true" path="passwd2" /><br>
+					<form:errors path="passwd2" style="color:red" /><br><br>
+					성명 :
+					<form:input type="text" path="username" maxlength="10" /><br>
+					<form:errors path="username" style="color:red" /><br><br>
+					닉네임 :
+					<form:input type="text" path="nickname" /><br>
+					<form:errors path="nickname" style="color:red" /><br><br>
+					연락처 :
+					<form:input type="text" path="phone" /><br>
+					<form:errors path="phone" style="color:red" /><br><br>
+			
+					<form:button type="submit">회원가입</form:button>
+					<input type="button" value="취소" onclick="location.href='/'" />
+				</form:form>
+		</div>
+	</div>
 </body>
 
 </html>
